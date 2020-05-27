a72b61348c623a67111c5a03b4ef88231a7c93e1
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-05-27 21:39:32 +0800

    new

--- src/main/java/com/zsw/utils/UserStaticURLUtil.java
+++ src/main/java/com/zsw/utils/UserStaticURLUtil.java
@@ -22,17 +22,9 @@ public class UserStaticURLUtil {
 
     public  static  final String userController_checkRememberToken = CommonStaticWord.System_Url + "/checkRememberToken";
 
-    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    public  static  final String  userController_selectUserCompany = "/selectUserCompany";
+    public  static  final String  userController_getUserCompanys = "/getUserCompanys";
 
-    public  static  final String companyController = "/company";
-    public  static  final String companyController_getUserCompanys = "/getUserCompanys";
-    public  static  final String companyController_selectUserCompany = "/selectUserCompany";
-
-    public  static  final String companyController_companysPage = "/companysPage";
-    public  static  final String companyController_newCompany = "/newCompany";
-    public  static  final String companyController_getCompany = "/getCompany";
-    public  static  final String companyController_updateCompany = "/updateCompany";
-    public  static  final String companyController_deleteCompany = "/deleteCompany";
 
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
@@ -50,7 +42,26 @@ public class UserStaticURLUtil {
 
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+    /*
+    *
+    *
+    * */
     public  static  final String admin = "/admin";
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    public  static  final String companyController = admin + "/company";
+    public  static  final String companyController_getUserCompanys = "/getUserCompanys";
+    public  static  final String companyController_selectUserCompany = "/selectUserCompany";
+
+    public  static  final String companyController_companysPage = "/companysPage";
+    public  static  final String companyController_newCompany = "/newCompany";
+    public  static  final String companyController_getCompany = "/getCompany";
+    public  static  final String companyController_updateCompany = "/updateCompany";
+    public  static  final String companyController_deleteCompany = "/deleteCompany";
+
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     public  static  final String adminUserController = admin + "/adminUser";
 
     public  static  final String adminUserController_login = "/login";
@@ -58,4 +69,30 @@ public class UserStaticURLUtil {
     public  static  final String adminUserController_loginOut = "/loginOut";
     public  static  final String adminUserController_checkRememberToken = CommonStaticWord.System_Url + "/checkRememberToken";
 
+
+
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    public  static  final String adminOperationUserController = admin + "/adminOperationUser";
+
+    public  static final String adminOperationUserController_newUser = "/newUser";
+    public  static  final String adminOperationUserController_getUser = "/getUser";
+    public  static  final String adminOperationUserController_updateUser = "/updateUser";
+    public  static  final String adminOperationUserController_batchBan = "/batchBan";
+    public  static  final String adminOperationUserController_usersPage = "/usersPage";
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    public  static  final String adminOperationDepartmentController = admin + "/adminOperationDepartment";
+
+    public  static  final String adminOperationDepartmentController_departmentsPage = "/departmentsPage";
+    public  static  final String adminOperationDepartmentController_newDepartment = "/newDepartment";
+    public  static  final String adminOperationDepartmentController_getDepartment = "/getDepartment";
+    public  static  final String adminOperationDepartmentController_updateDepartment = "/updateDepartment";
+    public  static  final String adminOperationDepartmentController_deleteDepartment = "/deleteDepartment";
+    public  static  final String adminOperationDepartmentController_updateStatusDepartment = "/updateStatusDepartment";
+
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }

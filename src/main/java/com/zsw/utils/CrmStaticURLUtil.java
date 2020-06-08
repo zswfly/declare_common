fc31b4349347a790aa882387ccd453ab0efbffdb
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-06-08 22:09:23 +0800

    new

---
+++ src/main/java/com/zsw/utils/CrmStaticURLUtil.java
@@ -0,0 +1,31 @@
+package com.zsw.utils;
+
+/**
+ * Created by zhangshaowei on 2020/4/15.
+ */
+public class CrmStaticURLUtil {
+
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    public  static  final String supplierControler = "/supplierControler";
+    public  static  final String supplierControler_newSupplier = "/newSupplier";
+    public  static  final String supplierControler_getSupplier = "/getSupplier";
+    public  static  final String supplierControler_updateSupplier = "/updateSupplier";
+    public  static  final String supplierControler_supplierPage = "/supplierPage";
+    public  static  final String supplierControler_batchBan = "/batchBan";
+
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    public  static  final String customerControler = "/customerControler";
+    public  static  final String customerControler_newCustomer = "/newCustomer";
+    public  static  final String customerControler_getCustomer = "/getCustomer";
+    public  static  final String customerControler_updateCustomer = "/updateCustomer";
+    public  static  final String customerControler_customerPage = "/customerPage";
+    public  static  final String customerControler_batchBan = "/batchBan";
+
+
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+}
